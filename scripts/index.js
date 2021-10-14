cd40a2a6f76d9a8ada92c5e197f27776c7377aee
Sveta SA <justforsysa@gmail.com>
2021-10-14 18:34:58 +1200

    feat: изменить загрузку шести карточек с помощью js и template

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,40 @@
 const modalWindow = document.querySelector('.popup'); //нохожу попап в DOM
 const modalWindowCloseBtn = modalWindow.querySelector('.popup__button-close'); //нахожу кнопку закрытия попапа
 const profileInfoSection = document.querySelector('.profile'); //нахожу секцию с профилем
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 const profileName = profileInfoSection.querySelector('.info__name');
 const profileInfoAbout = profileInfoSection.querySelector('.info__about');
 const editProfileInfoBtn = profileInfoSection.querySelector('.info__button-edit'); //нахожу кнопку редактирования инфы
 const formProfile = modalWindow.querySelector('.form'); //нахожу форму
 const formInputName = formProfile.querySelector('.form__input_value_name'); //нахожу поля
 const formInputAbout = formProfile.querySelector('.form__input_value_about');
+const cardTemplate = document.querySelector('#element-item-template').content;
+const cardContainer = document.querySelector('.elements');
 //открытие формы
 function openForm() {
   modalWindow.classList.add('popup_opened');
@@ -34,6 +62,16 @@ function saveProfileInfo(evt) {
   profileInfoAbout.textContent = formInputAbout.value;
   closeForm();
 }
+//функциональные выражения
+const renderCard = (card) => {
+  const cardItem = cardTemplate.cloneNode(true);
+  const cardCaption = cardItem.querySelector('.element__title');
+  const cardLink = cardItem.querySelector('.element__card');
+  cardCaption.textContent = card.name;
+  cardLink.src = card.link;
+  cardContainer.append(cardItem);
+};
+initialCards.forEach(renderCard);
 //слушатели событий
 modalWindow.addEventListener('click', onOverlayClick);
 modalWindowCloseBtn.addEventListener('click', closeForm);
